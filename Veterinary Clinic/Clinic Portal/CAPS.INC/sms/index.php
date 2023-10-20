b9f026e0ab413fd263cff69613b897ce0d84cc97
JEM-YORONG <yorongjemar0@gmail.com>
2023-10-20 18:17:30 +0800

    update token

--- Veterinary Clinic/Clinic Portal/CAPS.INC/sms/index.php
+++ Veterinary Clinic/Clinic Portal/CAPS.INC/sms/index.php
@@ -4,15 +4,10 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Twilio\Rest\Client;
 
-$sid    = "AC49fba0b65605b2a656902f4b634a0c20";
-$token  = "011d22c217b509434e35ca95cc5021d5";
-$twilioNumber = "+12566935328";
-$verifiedNumber = "+639217214912";
-
 function send($number, $message)
 {
     $sid    = "AC49fba0b65605b2a656902f4b634a0c20";
-    $token  = "011d22c217b509434e35ca95cc5021d5";
+    $token  = "f74da1be1a25e84bedb5103327e33bc9";
     $twilioNumber = "+12566935328";
 
     $twilio = new Client($sid, $token);
@@ -29,31 +24,6 @@ function send($number, $message)
 
         echo "Message sent successfully";
     } catch (Exception $e) {
-        //echo "Error: " . $e->getMessage();
-    }
-}
-
-function sendinventory($message)
-{
-    global $sid;
-    global $token;
-    global $twilioNumber;
-    global $verifiedNumber;
-
-    $twilio = new Client($sid, $token);
-
-    // Send an SMS message
-    try {
-        $message = $twilio->messages->create(
-            $verifiedNumber, // Recipient phone number
-            array(
-                "from" => $twilioNumber, // Your Twilio phone number
-                "body" => $message
-            )
-        );
-
-        //echo "Message sent successfully";
-    } catch (Exception $e) {
-        //echo "Error: " . $e->getMessage();
+        echo "Error: " . $e->getMessage();
     }
 }
