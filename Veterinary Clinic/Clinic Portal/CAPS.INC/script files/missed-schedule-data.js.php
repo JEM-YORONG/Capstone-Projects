288aaabb98f4ea13ec208a0476a47287d0ebe1b1
JEM-YORONG <yorongjemar0@gmail.com>
2023-11-03 09:21:00 +0800

    missed-schedule-data.js.php added

---
+++ Veterinary Clinic/Clinic Portal/CAPS.INC/script files/missed-schedule-data.js.php
@@ -0,0 +1,24 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script>
+    function submitData(action) {
+        $(document).ready(function() {
+            var data = {
+                action: action,
+                id: $("#ownerId").val(),
+                name: $("#name").val(),
+                date: $("#date").val(),
+                rowId: $("#rowId").val(),
+            }
+
+            $.ajax({
+                url: 'missed-schedule-function.php',
+                type: 'post',
+                data: data,
+
+                success: function(response) {
+                    alert(response);
+                }
+            });
+        });
+    }
+</script>
