74fb05e3d34d54a8a44893ede8efbf0bce04f88b
JEM-YORONG <yorongjemar0@gmail.com>
2023-11-03 09:21:28 +0800

    schedule.data.js.php updates

--- Veterinary Clinic/Clinic Portal/CAPS.INC/script files/schedule.data.js.php
+++ Veterinary Clinic/Clinic Portal/CAPS.INC/script files/schedule.data.js.php
@@ -35,11 +35,19 @@
                 updateId: $("#ownerIdUpdate").val(),
                 updatedate: $("#dateUpdate").val(),
                 updatename: $("#nameUpdate").val(),
+                //default
                 updatepetname1: $("#petname1Update").val(),
                 updatepetname2: $("#petname2Update").val(),
                 updatepetname3: $("#petname3Update").val(),
                 updatepetname4: $("#petname4Update").val(),
                 updatepetname5: $("#petname5Update").val(),
+                //modified
+                updatepetname12: $("#petname1Update2").val(),
+                updatepetname22: $("#petname2Update2").val(),
+                updatepetname32: $("#petname3Update2").val(),
+                updatepetname42: $("#petname4Update2").val(),
+                updatepetname52: $("#petname5Update2").val(),
+                
                 updatetype: $("#typeUpdate").val(),
                 updateservice1: $("#service1Update").val(),
                 updateservice2: $("#service2Update").val(),
@@ -57,12 +65,11 @@
                 data: data,
 
                 success: function(response) {
-                    if(response == ""){
-                        //alert(response);
-                    }
-                    else{
-                        alert(response);
-                    }
+                    $("#petname1Update2").html(response);
+                    $("#petname2Update2").html(response);
+                    $("#petname3Update2").html(response);
+                    $("#petname4Update2").html(response);
+                    $("#petname5Update2").html(response);
                 }
             });
         });
