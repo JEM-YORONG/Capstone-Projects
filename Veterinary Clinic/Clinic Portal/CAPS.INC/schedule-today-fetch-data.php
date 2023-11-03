dd958fcd95aa64cedb6f915fd516be3b94f48a19
JEM-YORONG <yorongjemar0@gmail.com>
2023-11-03 09:24:36 +0800

    schedule-today-fetch-data.php updates

--- Veterinary Clinic/Clinic Portal/CAPS.INC/schedule-today-fetch-data.php
+++ Veterinary Clinic/Clinic Portal/CAPS.INC/schedule-today-fetch-data.php
@@ -42,7 +42,7 @@ if ($result) {
 ?>
             <div class="table-body">
                 <tr>
-                    <td><button type="button" class="done-button" onclick="rowStatus('<?php echo $row['id']; ?>'); submitData('statusDone')" style=" background-color: #00000000;
+                    <td><button type="button" class="done-button" onclick="rowStatus('<?php echo $row['id']; ?>');" style=" background-color: #00000000;
                             border-style: none;
                             color: #5a81fa;
                             cursor: pointer;">
@@ -53,7 +53,13 @@ if ($result) {
                     <td data-label="Notify" onclick="setMessageToday('<?php echo $row['ownername']; ?>',
                     '<?php echo $row['date']; ?>',
                     '<?php echo $row['petname']; ?>',
-                    '<?php echo $row['service']; ?>');"><button type="button" class="notify-button-1" style="background-color: #00000000; border-style: none; color: #5a81fa; cursor: pointer;" onclick="opensms(); infoSMS('<?php echo $row['date']; ?>', '<?php echo $row['number']; ?>', '<?php echo $row['ownername']; ?>', '<?php echo $row['petname']; ?>');"><span class="material-symbols-outlined">reminder</span></button></td>
+                    '<?php echo $row['petname2']; ?>',
+                    '<?php echo $row['petname3']; ?>',
+                    '<?php echo $row['petname4']; ?>',
+                    '<?php echo $row['petname5']; ?>',
+                    '<?php echo $row['service']; ?>',
+                    '<?php echo $row['service2']; ?>',
+                    '<?php echo $row['service3']; ?>');"><button type="button" class="notify-button-1" style="background-color: #00000000; border-style: none; color: #5a81fa; cursor: pointer;" onclick="opensms(); infoSMS('<?php echo $row['date']; ?>', '<?php echo $row['number']; ?>', '<?php echo $row['ownername']; ?>', '<?php echo $row['petname']; ?>');"><span class="material-symbols-outlined">reminder</span></button></td>
                     <td><?php echo $row['date']; ?></td>
                     <td data-label="Owner Name"><?php echo $row['ownername']; ?></td>
 
@@ -69,9 +75,14 @@ if ($result) {
                     '<?php echo $row['date']; ?>',
                     '<?php echo $row['ownername']; ?>',
                     '<?php echo $row['petname']; ?>',
+                    '<?php echo $row['petname2']; ?>',
+                    '<?php echo $row['petname3']; ?>',
+                    '<?php echo $row['petname4']; ?>',
+                    '<?php echo $row['petname5']; ?>',
                     '<?php echo $row['service']; ?>',
-                    '<?php echo $row['number']; ?>'
-                    );"><span class="material-symbols-outlined">edit</span></button></td>
+                    '<?php echo $row['service2']; ?>',
+                    '<?php echo $row['service3']; ?>',
+                    '<?php echo $row['number']; ?>');"><span class="material-symbols-outlined">edit</span></button></td>
 
                     <td> <button type="button" class="delete-button" onclick="openFormDelete(); deleteRow('<?php echo $row['id']; ?>')"><span class="material-symbols-outlined">delete</span></button></td>
                 </tr>
