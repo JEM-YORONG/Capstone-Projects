4db53a05da5e74a35e189134782e149f4e5ced3e
JEM-YORONG <yorongjemar0@gmail.com>
2023-11-03 09:24:12 +0800

    schedule-function.php updates

--- Veterinary Clinic/Clinic Portal/CAPS.INC/schedule-function.php
+++ Veterinary Clinic/Clinic Portal/CAPS.INC/schedule-function.php
@@ -17,6 +17,9 @@ if (isset($_POST["action"])) {
     if ($_POST["action"] === "statusDone") {
         statusDone();
     }
+    if ($_POST["action"] === "getName") {
+        display();
+    }
 }
 
 function addAppointment()
@@ -85,17 +88,64 @@ function updateAppointment()
     $id = $_POST["updateId"];
     $date = $_POST["updatedate"];
     $name = $_POST["updatename"];
+
+    //default
     $petname = $_POST["updatepetname1"];
     $petname2 = $_POST["updatepetname2"];
     $petname3 = $_POST["updatepetname3"];
     $petname4 = $_POST["updatepetname4"];
     $petname5 = $_POST["updatepetname5"];
+    //modified
+    $petnameUpdate = $_POST["updatepetname12"];
+    $petname2Update = $_POST["updatepetname22"];
+    $petname3Update = $_POST["updatepetname32"];
+    $petname4Update = $_POST["updatepetname42"];
+    $petname5Update = $_POST["updatepetname52"];
+    //holders
+    $p1 = $petnameUpdate;
+    $p2 = $petname2Update;
+    $p3 = $petname3Update;
+    $p4 = $petname4Update;
+    $p5 = $petname5Update;
+    //empty value checker
+    if ($petnameUpdate == "") {
+        $p1 = $petname;
+    }
+    if ($petname2Update == "") {
+        $p2 = $petname2;
+    }
+    if ($petname3Update == "") {
+        $p3 = $petname3;
+    }
+    if ($petname4Update == "") {
+        $p4 = $petname4;
+    }
+    if ($petname5Update == "") {
+        $p5 = $petname5;
+    }
+    if ($petnameUpdate == "r") {
+        $p1 = "";
+    }
+    if ($petname2Update == "r") {
+        $p2 = "";
+    }
+    if ($petname3Update == "r") {
+        $p3 = "";
+    }
+    if ($petname4Update == "r") {
+        $p4 = "";
+    }
+    if ($petname5Update == "r") {
+        $p5 = "";
+    }
+
     $service1 = $_POST["updateservice1"];
     $service2 = $_POST["updateservice2"];
     $service3 = $_POST["updateservice3"];
     $number = $_POST["updatenumber"];
 
-    $query = "UPDATE schedule SET ownername = '$name', petname = '$petname', petname2 = '$petname2', petname3 = '$petname3', petname4 = '$petname4', petname5 = '$petname5', service = '$service1', service2 = '$service2', service3 = '$service3', date = '$date', number = '$number' WHERE id = '$id'";
+    $query = "UPDATE schedule SET ownername = '$name', petname = '$p1', petname2 = '$p2', petname3 = '$p3', petname4 = '$p4', petname5 = '$p5', service = '$service1', service2 = '$service2', service3 = '$service3', date = '$date', number = '$number' WHERE id = '$id'";
+
     mysqli_query($conn, $query);
     mysqli_close($conn);
     echo "Schedule Updated Successfully.";
@@ -141,4 +191,33 @@ function sendSMS()
     require 'sms\index.php';
 
     echo send($smsNumber, $smsMessage);
+}
+
+function display()
+{
+    global $conn;
+
+    if (isset($_POST['updatename'])) {
+        $name = $_POST["updatename"];
+
+        //echo $name;
+
+        $query = "SELECT * FROM pet WHERE CONCAT(ownerfirstname, ' ',ownerlastname) = '$name'";
+        $result = mysqli_query($conn, $query);
+
+        if ($result) {
+            echo "<option value=''>Select a pet</option>";
+            echo "<option value='r'>Remove</option>";
+            while ($row = mysqli_fetch_assoc($result)) {
+                $petname = $row["petname"];
+                echo "<option value='$petname'>$petname</option>";
+            }
+        } else {
+            echo "Error: " . mysqli_error($conn);
+        }
+    } else {
+        echo "Customer name not provided.";
+    }
+
+    mysqli_close($conn);
 }
